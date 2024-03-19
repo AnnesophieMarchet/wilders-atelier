5fce15c397b63f093fdaa572d392ddd6fccf84d2
Anne-sophie Marchet <annesophiemarchet@gmail.com>
2024-03-19 12:04:04 +0100

    boucle for in wilders

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-node_modules;
+import chalk from "chalk";
+
+const log = console.log;
+
+// log(chalk.blue("Hello") + " World" + chalk.red("!"));
+
+let wilders = ["Adeline", "Thien", "Charlot", "Laura"];
+let colors = ["#FF8800", "	#FA8072", "#8727B3", "#087320"];
+
+// for (let i = 0; i < wilders.length; i++) {
+//   const wilderName = chalk.hex(colors[i])(wilders[i]);
+//   console.log(wilderName);
+// }
+for (const i in wilders) {
+  const wilderName = chalk.hex(colors[i])(wilders[i]);
+  console.log(wilderName);
+}
+// console.log((wilders[0] = chalk.cyanBright));
